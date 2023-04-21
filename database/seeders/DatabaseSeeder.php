167ef2b79e5f2349296b481b88e2309d23ac836e
Oakekywe <oakekywephyohann99@gmail.com>
2023-04-21 21:17:08 +0630

    bugs

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,16 +18,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        
-        $mgmg= User::factory()->create(['name'=>'mgmg', 'username'=>'mgmg']);
-        $aungaung= User::factory()->create(['name'=>'aungaung', 'username'=>'aungaung']);
+        User::factory()->create(['name' => 'admin', 'email' => 'admin@gmail.com', 'is_admin' => 1, 'password' => 'qwer1234']);
+        $mgmg = User::factory()->create(['name' => 'mgmg', 'username' => 'mgmg']);
+        $aungaung = User::factory()->create(['name' => 'aungaung', 'username' => 'aungaung']);
 
-        $frontend= Category::factory()->create(['name'=>"frontend", 'slug'=>'frontend']);
-        $backend= Category::factory()->create(['name'=>"backend", 'slug'=>'backend']);
+        $frontend = Category::factory()->create(['name' => "frontend", 'slug' => 'frontend']);
+        $backend = Category::factory()->create(['name' => "backend", 'slug' => 'backend']);
 
-        Blog::factory(2)->create(["category_id"=>$frontend->id, 'user_id'=>$mgmg->id]);
-        Blog::factory(2)->create(["category_id"=>$backend->id, 'user_id'=>$aungaung->id]);
-
-       
+        Blog::factory(4)->create(["category_id" => $frontend->id, 'user_id' => $mgmg->id]);
+        Blog::factory(4)->create(["category_id" => $backend->id, 'user_id' => $aungaung->id]);
     }
 }
--- resources/views/components/form/input.blade.php
+++ resources/views/components/form/input.blade.php
@@ -2,6 +2,6 @@
 <x-form.input-wrapper>
     <x-form.lable :name="$name"/>
     <input type="{{$type}}" name="{{$name}}" class="form-control" value="{{old($name, $value)}}"
-    id="{{$name}}" >    
+    id="{{$name}}" required>    
     <x-error :name="$name" /> 
 </x-form.input-wrapper>
